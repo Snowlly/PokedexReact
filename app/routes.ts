45f020a45e31ee18278c3486113548c63e2bb8ff
Snowlly <evanmartin2001@gmail.com>
2025-06-05 13:03:45 +0200

    pb de routes

--- app/routes.ts
+++ app/routes.ts
@@ -3,9 +3,8 @@ import { type RouteConfig, index, route, file } from "@react-router/dev/routes";
 export default [
     index("routes/home.tsx"),
 
-    // Route /pokemon (avec composant "layout")
     route("pokemon", "routes/pokemon.tsx", [
-        index("routes/pokemon/index.tsx"), // /pokemon
-        route(":pokemonName", "routes/pokemon/$pokemonName.tsx"), // /pokemon/:pokemonName
+        index("routes/pokemon/index.tsx"),
+        route(":pokemonName", "routes/pokemon/$pokemonName.tsx"),
     ]),
 ] satisfies RouteConfig;
